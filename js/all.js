038c3edcd023bceacd1120c9f5e320c09a791805
bourgondien <bente@bourgondien.nl>
2017-03-30 14:21:32 +0200

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,12 @@
+/* basic onload function */
+
+window.onload = function() {		
+		console.log('document finished loading!')  
+		
+		/* basic click event listener (no jquery required) */
+		addEventListener("click", function(event) {
+		
+			console.log('you have clicked!')  		
+		
+		});
+}
